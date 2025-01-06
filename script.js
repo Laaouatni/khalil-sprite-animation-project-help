ec59dfb2bd260c3654d4b5df8dc15b5a59164fb2
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-01-06 22:26:42 +0100

    save

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+const genericPositions = {
+  x: [20, -89, -195, -304, -408, -513, -620, -720, -820, -920, -1030],
+  y: [-109, 216, 323, 539, 649, 752, 856, 964],
+};
+
 const imgData = {
   nameImage: {
     url: "./imgs/0.jpeg",
-    genericPositions: {
-      x: [20, -89, -195, -304, -408, -513],
-      y: [-109, 216, 323, 539, 649, 752, 856, 964],
-    },
     numberItems: {
       x: [6, 8, 8, 12, 5, 3, 4, 4, 2],
     },
@@ -21,71 +22,46 @@ const imgData = {
       x: [6, 8, 8, 10, 4, 3, 4, 2, 3],
     },
   },
-};
+// };
 
-const appComponents = {
-  imageSelect: document.querySelector("#imageSelector"),
-  imageShow: document.querySelector("#imageShow"),
-};
+// const appComponents = {
+//   imageSelect: document.querySelector("#imageSelector"),
+//   imageShow: document.querySelector("#imageShow"),
+// };
 
-Object.entries(imgData).forEach(([key, value]) => {
-  const option = document.createElement("option");
-  option.value = value.url;
-  option.textContent = key;
-  appComponents.imageSelect.appendChild(option);
-});
+// Object.entries(imgData).forEach(([key, value]) => {
+//   const option = document.createElement("option");
+//   option.value = value.url;
+//   option.textContent = key;
+//   appComponents.imageSelect.appendChild(option);
+// });
 
-const imageSize = {
-  width: 140,
-  height: 140,
-};
+// const imageSize = {
+//   width: 140,
+//   height: 140,
+// };
 
-const stylingStrings = {
-  defineHeightWidth: `width:${imageSize.width}px; height: ${imageSize.height}px;`,
-};
+// const stylingStrings = {
+//   defineHeightWidth: `width:${imageSize.width}px; height: ${imageSize.height}px;`,
+// };
 
-appComponents.imageShow?.setAttribute(
-  "style",
-  `${stylingStrings.defineHeightWidth} background: url(${imgData.nameImage.url}) 0px 0px`,
-);
+// appComponents.imageShow?.setAttribute(
+//   "style",
+//   `${stylingStrings.defineHeightWidth} background: url(${imgData.nameImage.url}) 0px 0px`,
+// );
 
-const initialOption = appComponents.imageSelect?.querySelector("option");
+// const initialOption = appComponents.imageSelect?.querySelector("option");
 
-const arrayPositions = Object.values(
-  imgData[initialOption?.textContent].positions,
-).flat();
+// const arrayPositions = Object.values(
+//   imgData[initialOption?.textContent].positions,
+// ).flat();
 
-arrayPositions.forEach((position, index) => {
-  setTimeout(() => {
-    const [x, y] = position;
-    appComponents.imageShow?.setAttribute(
-      "style",
-      `${stylingStrings.defineHeightWidth} background: url(${imgData.nameImage.url}) ${x}px ${y}px`,
-    );
-  }, index * (1000 / 2));
-});
-
-// appComponents.imageSelect.addEventListener("change", (e) => {
-//   const selectedUrl = e.target.value;
-//   appComponents.imageShow?.setAttribute(
-//     "style",
-//     `${stylingStrings.defineHeightWidth} background-image: url(${selectedUrl})`,
-//   );
-
-//   const thisUsedOption = e.target.querySelector(
-//     `option[value='${selectedUrl}']`,
-//   );
-//   const arrayPositions = Object.values(
-//     imgData[thisUsedOption.textContent].positions,
-//   ).flat();
-
-//   arrayPositions.forEach((position, index) => {
-//     setTimeout(() => {
-//       const [x, y] = position;
-//       appComponents.imageShow?.setAttribute(
-//         "style",
-//         `${stylingStrings.defineHeightWidth} background-image: url(${selectedUrl}); background-position: ${x}px ${y}px`,
-//       );
-//     }, index * (1000 / 2));
-//   });
+// arrayPositions.forEach((position, index) => {
+//   setTimeout(() => {
+//     const [x, y] = position;
+//     appComponents.imageShow?.setAttribute(
+//       "style",
+//       `${stylingStrings.defineHeightWidth} background: url(${imgData.nameImage.url}) ${x}px ${y}px`,
+//     );
+//   }, index * (1000 / 2));
 // });
